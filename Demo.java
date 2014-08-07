e40193ae3c96a5ab7d8e24f0673114859a408012
rohan_zhang <zj.sz.zhangqi@163.com>
2014-08-07 22:54:57 +0800

    我提交了一个Demo.java文件

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package com.zhangqi.demo;
+
+public class Demo {
+	public static void main(String[] args) {
+		String name = "123";
+		final String str = name;
+		System.out.println(str);
+
+		System.out.println(str);
+	}
+}
